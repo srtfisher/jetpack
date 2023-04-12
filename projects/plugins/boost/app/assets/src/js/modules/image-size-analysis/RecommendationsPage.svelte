0a4d8b3d47565973e3402722e049a30c0abd7be4
Nauris Pūķis <hi@pyronaur.com>
2023-04-12 15:22:15 +0300

    Add recommendations page to the router

---
+++ projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/RecommendationsPage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Header from './recommendations/Header.svelte';
+	import Pagination from './recommendations/Pagination.svelte';
+	import Table from './recommendations/Table.svelte';
+</script>
+
+<div class="recommendations-page">
+	<Header />
+	<Table />
+	<Pagination />
+</div>
+
+<style lang="scss">
+	.recommendations-page {
+		display: grid;
+		gap: 1rem;
+	}
+</style>
