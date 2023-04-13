07832b68274a94ce78f4dd3f4d0adc20217bbfe7
Nauris Pūķis <hi@pyronaur.com>
2023-04-13 18:23:05 +0300

    Add button on hover

--- projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/recommendations/TableRow.svelte
+++ projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/recommendations/TableRow.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import { ImageMeta } from '../ApiMock';
 	import TableRowExpanded from './TableRowExpanded.svelte';
+	import TableRowHover from './TableRowHover.svelte';
 
 	export let data: ImageMeta;
 	let expanded = false;
+	let hover = Math.random() > 0.5;
 </script>
 
-<div class="table-row">
+<div
+	class="table-row"
+	on:mouseenter={() => ( hover = true )}
+	on:mouseleave={() => ( hover = false )}
+>
 	<div class="thumbnail">
 		<img src={data.thumbnail} alt={data.title} width="64" height="64" />
 	</div>
@@ -14,12 +20,20 @@
 		<div>{data.title}</div>
 		<div>{data.urlPreview}</div>
 	</div>
-	<div class="device">
-		<img src={data.deviceIcon} alt={data.deviceType} />
-	</div>
-	<div class="page">
-		<a href={data.pageLink}>{data.pageTitle}</a>
-	</div>
+
+	{#if hover}
+		<div class="hover">
+			<TableRowHover />
+		</div>
+	{:else}
+		<div class="device">
+			<img src={data.deviceIcon} alt={data.deviceType} />
+		</div>
+		<div class="page">
+			<a href={data.pageLink}>{data.pageTitle}</a>
+		</div>
+	{/if}
+
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<div class="chevron" on:click={() => ( expanded = ! expanded )}>
 		<span>{expanded ? '▲' : '▼'}</span>
@@ -38,6 +52,7 @@
 		justify-content: space-between;
 		align-items: center;
 		gap: 1rem;
+		height: 150px;
 	}
 
 	.table-row > div:nth-child( 2 ) {
@@ -48,6 +63,11 @@
 	.chevron {
 		cursor: pointer;
 		text-align: right;
+	}
+
+	.hover {
+		// Replacing 2 columns, span 2
+		grid-column: span 2;
 	}
 
 	@media ( max-width: 768px ) {
---
+++ projects/plugins/boost/app/assets/src/js/modules/image-size-analysis/recommendations/TableRowHover.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Button from '../../../elements/Button.svelte';
+</script>
+
+<div class="hover">
+	<p>Resize this image to match the size of the screen</p>
+
+	<div class="button-container">
+		<Button>Edit Image</Button>
+	</div>
+</div>
+
+<style lang="scss">
+	.hover {
+		display: flex;
+	}
+	p {
+		flex-grow: 1;
+	}
+</style>
